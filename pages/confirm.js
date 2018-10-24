20bde893e04ccc1e4a161b0a9a4008d6a61099ab
Joshua Sprow <joshuasprow@Joshuas-MacBook-Pro-2.local>
2018-10-23 20:25:23 -0400

    added some markup to confirm.js

--- pages/confirm.js
+++ pages/confirm.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Meta from '../components/meta';
 import { withRouter } from 'next/router';
+import Row from '../components/row';
 
 class Confirm extends React.PureComponent {
   render() {
-    return <Meta>confirm</Meta>;
+    return (
+      <Meta>
+        <p>
+          You have <strong>70</strong> loyalty points!
+        </p>
+        <p>Would you like to apply these points to your parking pass?</p>
+        <Row>
+          <button>Yes</button>
+          <button>No</button>
+        </Row>
+      </Meta>
+    );
   }
 }
 
